8b758f95f73aee6e6b7faa3d9fe87c3776d3bcce
CSamuelLents <sam@samlents.com>
2021-05-28 12:21:41 -0400

    formatting

--- import-resource-group.sh
+++ import-resource-group.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z "$ARM_SUBSCRIPTION_ID" || -z "$ARM_RESOURCE_GROUP" ]]; then
-    echo "Required env variables not defined!"
+    echo "Required env variables not defined"
     echo "Run the following and try again:"
     echo "    - export ARM_SUBSCRIPTION_ID=<your_subscription_id>"
     echo "    - export ARM_RESOURCE_GROUP=<your_resource_group_name>"
